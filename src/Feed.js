08c7a95cf0b4511b67264098f545fb30dfb63bfc
Liu von Engelbrechten <vonengelbrechten.l@northeastern.edu>
2023-10-15 13:44:19 -0400

    connected supabase

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,9 @@
+import supabase from "./supabase.js";
+
 export default function Feed(){
-    
+    const query = supabase.from('test').select(`id, content, created_at`)
+
     return <p>Hello</p>
+
+
 }
